44156f75ee19013329855c324c6cf751a73386a3
Loey <0brigadeiro1213@gmail.com>
2022-07-09 17:43:03 -0300

    Aula 1 e Aula 2 DIO

---
+++ modulo 1/aula 1.py
@@ -0,0 +1,7 @@
+#Aprenda o que é a linguagem Python e como configurar o ambiente de desenvolvimento
+
+print('meu primeiro programa em python')
+a = 2
+b = 3
+soma = a + b
+print(soma)
---
+++ modulo 1/aula 2.py
@@ -0,0 +1,29 @@
+#O que são variáveis e como manipulá-las através de operadores aritméticos e interação com usuário
+
+#a = 10
+#b = 7
+a = int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+
+soma = a + b
+sub = a - b
+multi = a * b
+div = a / b
+resto = a % b
+
+# print('sua soma é: ' + str(soma))
+# conversão
+# soma = str(soma)
+# print(type(soma))
+
+# print('sua subtração é: ' + str(sub))
+# print('sua multiplicação é: ' + str(multi))
+# print('sua divisão é: ' + str(div))
+# print('seu resto é: ' + str(resto))
+
+# \n é o enter
+print ('Soma: {soma}. \nSubtração: {sub}. \nMultiplicação: {multi}. \nDivisão: {div}. \nResto: {resto}' .format(soma=soma,
+                sub=sub,
+                multi=multi,
+                div=div,
+                resto=resto))
