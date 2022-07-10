d495a60643040393f2cc810bb33f8eb94c45c36e
Loey <102442925+loeycism@users.noreply.github.com>
2022-07-10 18:46:33 -0300

    IF como repetição

--- modulo 1/aula 3.4.py
+++ modulo 1/aula 3.4.py
@@ -1,22 +1,24 @@
+#aula 3 parte 4
+#IF como repetição
 
 a = int(input('primeiro bimestre: '))
-while a > 10:
+if a > 10:
     a = int(input('Você digitou errado. Digite o valor correto: ')) 
 b = int(input('segundo bimestre: '))
-while b > 10:
+if b > 10:
     b = int(input('Você digitou errado. Digite o valor correto: '))
 c = int(input('terceiro bimestre: '))
-while c > 10:
+if c > 10:
     c = int(input('Você digitou errado. Digite o valor correto: '))
 d = int(input('quarto bimestre: '))
-while d > 10:
+if d > 10:
     d = int(input('Você digitou errado. Digite o valor correto: '))
 media = (a + b + c + d) / 4
 
-print('media: {}' .format(media))
+print('media: {}' .ifmat(media))
 
 
-# while a <= 10 and b <= 10 and c <= 10 and d <= 10:
-#     print('media: {}' .format(media))
+# if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+#     print('media: {}' .ifmat(media))
 # else:
-#     print('Foi informado um número errado')
+#     print('Foi inifmado um número errado')
