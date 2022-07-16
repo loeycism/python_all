d181921bb0a4be66a68b1b535fff2176c65d4092
Loey <102442925+loeycism@users.noreply.github.com>
2022-07-16 16:27:41 -0300

    IF, ELSE + or

--- course_DIO/aula_3/aula3_3.py
+++ course_DIO/aula_3/aula3_3.py
@@ -2,7 +2,7 @@ a = int(input('entre com o primeiro valor: '))
 b = int(input('entre com o segundo  valor: '))
 resto_a = a % 2
 resto_b= b % 2
-
+ 
 if resto_a == 0 or resto_b == 0:
     print('foi digitado um número par')
 else:
